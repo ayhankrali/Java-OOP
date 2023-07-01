b3aef9cd44ffcf8d8613a8456fb60b87cf813ea2
Ayhan Krala <codewolf27@gmail.com>
2023-07-01 16:08:25 +0300

    adding files

---
+++ OOP-exercises/inheritance/animals/Dog.java
@@ -0,0 +1,11 @@
+package inheritance.animals;
+
+public class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void makeSound() {
+        System.out.println("The dog barks.");
+    }
+}
